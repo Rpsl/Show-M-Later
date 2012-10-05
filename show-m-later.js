783b44221cdf6e528491d3e1f43d6816ce8bc115
Rpsl <gmail@rpsl.info>
2012-10-06 00:53:52 +0400

    comments

--- show-m-later.js
+++ show-m-later.js
@@ -2,20 +2,20 @@
  * Set unread
  * @type {Boolean}
  */
-var MARK_UNREAD     = true;
+var MARK_UNREAD = true;
 
 /**
  * Set stared
  * @type {Boolean}
  */
-var MARK_STARRED    = false;
+var MARK_STARRED = false;
 
 /**
  * Send log email to this email.
  * False for disable.
  * @type {String}
  */
-var SEND_LOG        = false;
+var SEND_LOG = false;
 
 
 /**
@@ -24,14 +24,14 @@ var SEND_LOG        = false;
  * @type {Array}
  */
 // var labels   = ["Later", "Guys", "Please", "Write", "Translation"];
-var labels      = ["Отложить", "Отложить/Нa 6 часов", "Отложить/На 1 день", "Отложить/На 2 дня"];
+var labels = ["Отложить", "Отложить/Нa 6 часов", "Отложить/На 1 день", "Отложить/На 2 дня"];
 
 /**
  * Day factor: 0.25*1 = 6 hours, 1*1 = 1 day.
  * Depends on : var labels
  * @type {Array}
  */
-var labels_int  = [0, 0.25, 1, 2];
+var labels_int = [0, 0.25, 1, 2];
 
 
 /**
@@ -39,16 +39,16 @@ var labels_int  = [0, 0.25, 1, 2];
  */
 function setup()
 {
-    for (i = 0; i < labels.length; i++)
+    for( i = 0; i < labels.length; i++ )
     {
-        GmailApp.createLabel(labels[i]);
+        GmailApp.createLabel( labels[i] );
     }
 }
 
 // Don't modify
-var messages    = {};
-var db          = ScriptDb.getMyDb();
-var counter     = 0;
+var messages = {};
+var db = ScriptDb.getMyDb();
+var counter = 0;
 
 function run()
 {
@@ -57,108 +57,113 @@ function run()
     // каунтер в нашем случае, это примитивные часы. начинается с 1, плюсуется до 24
     // потом обнуляется.
     // @todo проверить, что у нас действительно 24 часа, а не 23 или 25
-    counter = getData('counter', false);
+    counter = getData( 'counter', false );
 
     if ( counter == false )
     {
-        setData('counter', 1);
+        setData( 'counter', 1 );
         counter = 1;
     }
 
     // проходим по нашим лейблам и добавляем письма в стек
-    for (i = 0; i < labels.length; i++)
+    for( i = 0; i < labels.length; i++ )
     {
-        var label   = GmailApp.getUserLabelByName(labels[i]);
-        var mess    = label.getThreads();
+        var label = GmailApp.getUserLabelByName( labels[i] );
+        var mess = label.getThreads();
 
-        if (mess.length > 0)
+        if ( mess.length > 0 )
         {
-            Logger.log('%s %s %s', i, mess.length, counter );
-            moveMessages(i, mess);
+            Logger.log( 'Folder: %s | Threads: %s | Counter: %s', i, mess.length, counter );
+            // для упрощения кода вынес в отдельную ф-цию
+            moveMessages( i, mess );
         }
     }
 
-    if( SEND_LOG )
+    if ( SEND_LOG )
     {
-        MailApp.sendEmail( SEND_LOG, "Script Log", Logger.getLog());
+        MailApp.sendEmail( SEND_LOG, "Script Log", Logger.getLog() );
     }
 
     counter++;
 
-    if (counter > 24)
+    if ( counter > 24 )
     {
         counter = 0;
     }
 
-    setData('counter', counter);
+    setData( 'counter', counter );
 }
 
-function moveMessages(label_int, mess)
+function moveMessages( label_int, mess )
 {
     messages = {};
     var i;
-    for (i = 0; i < mess.length; i++)
-    {
 
-        var key = mess[i].getId();
-        var check = getData(key);
+    // проходим список писем, проверяем какие уже есть в дб
+    // новые кладем в дб, со значениями времени их жизни ( factor * 24 )
+    for( i = 0; i < mess.length; i++ )
+    {
+        var key     = mess[i].getId();
+        var check   = getData( key, false );
 
         if ( check === false )
         {
-            Logger.log('check undefined');
-            setData(key, labels_int[label_int]*24);
-            check = labels_int[label_int]*24;
+            Logger.log( 'new mail - %s', check );
+
+            setData( key, labels_int[label_int] * 24 );
+            check   = labels_int[label_int] * 24;
         }
-        messages[key] = check-1;
+
+        messages[key] = check - 1;
         delete check, key;
     }
-    Logger.log( messages );
 
+    // Logger.log( messages );
+
+    // перебираем все треды из стэка и перемещаем их при необходимости
     for( var k in messages )
     {
-
         var inbox = false;
+        var temp_msg = GmailApp.getThreadById( k );
 
-        Logger.log(labels[label_int]);
+        // Logger.log( labels[label_int] );
 
-        if( labels[label_int-1] === undefined || messages[k] <= 0 )
+        if ( labels[label_int - 1] === undefined || messages[k] <= 0 )
         {
-            Logger.log('inbox');
+            Logger.log( 'inbox' );
 
-            var temp_msg = GmailApp.getThreadById( k );
             var oldLabel = GmailApp.getUserLabelByName( labels[ label_int ] );
             temp_msg.moveToInbox();
 
-            if( MARK_UNREAD ) {
-                GmailApp.markThreadUnread(temp_msg);
+            if ( MARK_UNREAD )
+            {
+                GmailApp.markThreadUnread( temp_msg );
             }
 
-            if( MARK_STARRED ) {
+            if ( MARK_STARRED )
+            {
                 GmailApp.starMessage( temp_msg );
             }
 
 
-            oldLabel.removeFromThread(temp_msg);
-            removeData(k);
+            oldLabel.removeFromThread( temp_msg );
+            removeData( k );
             inbox = true;
 
         }
-        else if( messages[k] <= ( labels_int[label_int-1]*24 ) )
+        else if ( messages[k] <= ( labels_int[label_int - 1] * 24 ) )
         {
-            var temp_msg = GmailApp.getThreadById( k );
-
-            GmailApp.getUserLabelByName( labels[ label_int ] ).removeFromThread(temp_msg);
-            GmailApp.getUserLabelByName( labels[ label_int-1 ] ).addToThread(temp_msg);
-
+            GmailApp.getUserLabelByName( labels[ label_int ] ).removeFromThread( temp_msg );
+            GmailApp.getUserLabelByName( labels[ label_int - 1 ] ).addToThread( temp_msg );
 
             Logger.log( 'msg: %s, %s', temp_msg.getId(), temp_msg.getFirstMessageSubject() );
-            Logger.log( 'count: %s, move_count: %s (%s)', messages[k], ( labels_int[label_int-1]*24 ), labels[label_int] );
-            Logger.log( 'need move to - %s', labels[ label_int-1 ] );
+            Logger.log( 'count: %s, move_count: %s (%s)', messages[k], ( labels_int[label_int - 1] * 24 ), labels[label_int] );
+            Logger.log( 'need move to - %s', labels[ label_int - 1 ] );
         }
 
-        if( !inbox )
+        if ( !inbox )
         {
-            setData(k, messages[k]);
+            setData( k, messages[k] );
         }
     }
 }
@@ -175,12 +180,19 @@ function getData( key, raw )
 {
     raw = raw || false;
 
-    result = db.query({ key: key });
+    result = db.query( { key:key } );
 
-    while (result.hasNext()) {
+    while ( result.hasNext() )
+    {
         var ob = result.next();
 
-        if( raw ) { return ob; } else { return ob.value; }
+        if ( raw )
+        {
+            return ob;
+        } else
+        {
+            return ob.value;
+        }
     }
 
     return false;
@@ -190,12 +202,12 @@ function getData( key, raw )
  * Add data to Db
  *
  * @param {String} key
- * @param {String} value
+ * @param {String|int} value
  */
 function setData( key, value )
 {
     removeData( key );
-    var object = { key: key, value: value };
+    var object = { key:key, value:value };
     db.save( object )
 }
 
@@ -207,9 +219,9 @@ function removeData( key )
 {
     var obj = getData( key, true );
 
-    if( obj !== false )
+    if ( obj !== false )
     {
-        db.remove(obj);
+        db.remove( obj );
     }
 }
 
